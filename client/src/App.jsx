9d60c6535baa71a7d4653579caa0e7fae8004228
Amanjha8503 <99260113+AmanJha7@users.noreply.github.com>
2024-03-24 22:55:31 +0530

    Only Loggedin can access
    i Made a Sprit code after that only loggedin user can visit any url of our site.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePage from "./components/HomePage";
 import Error404 from "./components/Error404";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import AllUsers from "./components/AllUsers/AllUsers";
@@ -10,21 +10,29 @@ import Protected from "./Protected";
 import Confess from "./pages/confess/Confess";
 import ConfessByMe from "./pages/confessbyme/ConfessByMe";
 import ConfessionsForMe from "./pages/confessionforme/ConfessionForMe";
+
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
-        <Route path='/users' element={<Protected Component={AllUsers}/>}/>
-        <Route path='/users/:userId' element={<ViewProfile/>}/>
-        <Route path='/confess/:userId' element={<Confess/>}/>
-        <Route path='/confessbyme' element={<ConfessByMe/>}/>
-        <Route path='/confessforme' element={<ConfessionsForMe/>}/>
+        {isAuthenticated ? (
+          <>
+            <Route path="/users" element={<Protected Component={AllUsers} />} />
+            <Route path="/users/:userId" element={<Protected Component={ViewProfile} />} />
+            <Route path="/confess/:userId" element={<Protected Component={Confess} />} />
+            <Route path="/confessbyme" element={<Protected Component={ConfessByMe} />} />
+            <Route path="/confessforme" element={<Protected Component={ConfessionsForMe} />} />
+          </>
+        ) : (
+          <Navigate to="/auth/login" />
+        )}
         <Route path="/*" element={<Error404 />} />
       </Routes>
-
     </Router>
   );
 };
